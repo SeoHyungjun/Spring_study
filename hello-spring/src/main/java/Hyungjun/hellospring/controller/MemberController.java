7fdd423c7b913cc4e8e86ea3407db138c501fbfd
SeoHyungjun <wewwe96@naver.com>
2021-11-14 21:04:57 +0900

    FEAT : Add MemberController using Component Scan
    Register spring bean using component scan
    Add Component Scan
        - MemberRepository use @Repository
        - MemberService use @Service
        - MemberController use @Controller

---
+++ hello-spring/src/main/java/Hyungjun/hellospring/controller/MemberController.java
@@ -0,0 +1,25 @@
+package Hyungjun.hellospring.controller;
+
+import Hyungjun.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    // spring이 실행될 때 MemberController가 생성됨.
+    // 그때 생성자(Constructor)가 호출 됨
+    // Autowired로 되어있으면, spring이 sping container에 있는 memberSerivce를 가져다 연결
+    // 그러기 위해서 MemberService에 @Service를 추가해야 함
+    // 이러한 방식을 Dependency Injection라고 함
+    // @Controller, @Service, @Repository와 같이 설정하는건 컴포넌트 스캔 방식
+    // 자동?방식으로 스프링 빈을 등록하는 과정
+    // 스프링은 스프링 컨테이너에 스프링 빈을 등록할 때, 기본적으로 싱글톤으로 등록(유일하게 하나만 등록해서 공유)
+    // 따라서 같은 빈이면 같은 인스턴스
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
--- hello-spring/src/main/java/Hyungjun/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/Hyungjun/hellospring/service/MemberService.java
@@ -3,15 +3,19 @@ package Hyungjun.hellospring.service;
 import Hyungjun.hellospring.domain.Member;
 import Hyungjun.hellospring.repository.MemberRepository;
 import Hyungjun.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     private final MemberRepository memberRepository; // = new MemoryMemberRepository();
 
     // 기존에 MemberService class를 만들 때 생성되던 memberRepository를 외부에서 넣어주는 방식으로 변경
     // memberService 입장에서 MemoryMemberRepository를 외부에서 넣어줌 == DI(Dependency Injection)라고 함
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
