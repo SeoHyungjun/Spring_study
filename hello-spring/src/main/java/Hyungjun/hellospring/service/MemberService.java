79ce7bc83457fe1448dcc9201e3abe5bac711d74
SeoHyungjun <wewwe96@naver.com>
2021-11-26 19:04:55 +0900

    FEAT: Add list search function and page
    /members to display the list of registered customers in memberList.html
         - Use thymeleaf's th:each to print the ids and names of members stored in the model

--- hello-spring/src/main/java/Hyungjun/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/Hyungjun/hellospring/service/MemberService.java
@@ -43,7 +43,7 @@ public class MemberService {
     /**
      * 전체 회원 조회
      */
-    public List<Member> findByMembers() {
+    public List<Member> findMembers() {
         return memberRepository.finaAll();
     }
 
