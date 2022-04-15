d3006438bbf775260710cab13585b8127f258d11
SeoHyungjun <wewwe96@naver.com>
2022-04-15 19:32:55 +0900

    FEAT : Change to follow DIP(Dependency Inversion Principle) using AppConfig

--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -3,16 +3,19 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderSerivce;
-import hello.core.order.OrderServiceImpl;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderSerivce orderSerivce = new OrderServiceImpl();
+
+        AppConfig appConfig = new AppConfig();
+
+        //MemberService memberService = new MemberServiceImpl();
+        //OrderSerivce orderSerivce = new OrderServiceImpl();
+        MemberService memberService = appConfig.memberService();
+        OrderSerivce orderSerivce = appConfig.orderSerivce();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
--- core/src/test/java/hello/core/order/OrderServiceTest.java
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,16 +1,28 @@
 package hello.core.order;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.AopProxy;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderSerivce orderSerivce = new OrderServiceImpl();
+    //MemberService memberService = new MemberServiceImpl();
+    //OrderSerivce orderSerivce = new OrderServiceImpl();
+    MemberService memberService;
+    OrderSerivce orderSerivce;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderSerivce = appConfig.orderSerivce();
+    }
 
     @Test
     void createOrder() {
