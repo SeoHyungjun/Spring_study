4a71e508895bd8cd75bf10e61fcd92fb4cac4a6a
SeoHyungjun <wewwe96@naver.com>
2022-05-28 17:19:32 +0900

    FEAT : Add or exclude scan targets from ComponentScan using Include, Exclude Filters

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        //basePackages = "hello.core", //{"hello.core", "hello.service"}와 같이 여러개도 사용 가능
+        //basePackageClasses = AutoAppConfig.class //-> 상단 package hello.core로 설정됨
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
