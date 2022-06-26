8d725ffa075e3bf9a1d8791a0a7cbaa7f2f110bd
SeoHyungjun <wewwe96@naver.com>
2022-06-26 19:14:24 +0900

    FEAT : Use Lombok's @RequiredArgsConstructor to change to concise code without OrderServiceImpl's constructor
    If you add lombok's @RequiredArgsConstructor, it automatically creates a constructor by collecting final fields.
    Invisible to code, but actually callable

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,10 +3,12 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     //private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -14,11 +16,15 @@ public class OrderServiceImpl implements OrderService {
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     private final DiscountPolicy discountPolicy;
 
+    //lombok의 @RequiredArgsConstructor을 추가하면 final이 붙은 필드를 모아서 생성자를 자동으로 만들어준다
+    //코드에는 보이지 않지만 실제 호출 가능
+  능 /*
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+    */
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
